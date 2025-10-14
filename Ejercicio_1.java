52c0541d99b66896604836a2e81825281eb208f4
laurazdzz14 <laura.lcd14@gmail.com>
2025-10-14 18:12:47 -0500

    Add files via upload

---
+++ Ejercicio_1.java
@@ -0,0 +1,31 @@
+
+package secuenciales;
+
+/**
+ *
+ * @author Laura Cardona Dominguez 
+ */
+public class Ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Crear variables
+        String username="Camila";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        
+        //Proceso
+        sumaDulces= cofreUno + cofreDos;
+        
+        //Salida 
+        System.out.println(username + " ahora tiene un total de " + sumaDulces +" caramelos magicos");
+        
+        
+    }
+    
+}
---
+++ Ejercicio_2.java
@@ -0,0 +1,30 @@
+
+package secuenciales;
+
+/**
+ *
+ * @author Laura Cardona Dominguez
+ */
+public class Ejercicio_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Crear Variables
+        double PrimerReto= 3.5;
+        double SegundoReto= 4.2;
+        double TrecerReto= 2.8;
+        double sumaPorcentajes;
+        
+        //Proceso
+        sumaPorcentajes = (PrimerReto + SegundoReto + TrecerReto)/3;
+        
+        //Salida
+        System.out.println(" Tu promedio final es:" + sumaPorcentajes + " ¡Nivel Superado");
+        
+        
+    }
+    
+}
