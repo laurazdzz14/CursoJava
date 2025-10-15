05208ee7bea8134a50eaab2491ab5d58deae8f07
laurazdzz14 <laura.lcd14@gmail.com>
2025-10-15 17:28:26 -0500

    Update RetosJava.java

--- RetosJava.java
+++ RetosJava.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package retosjava;
+
 
 /**
  *
- * @author Estudiante
+ * @author Laura Cardona Dominguez
  */
-public class RetosJava {
+public class Ejercicio_3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Crear Variables
+         int salario=2000000;
+         double SaludMagica=2000000*0.04;
+         double ProteccionDePension=2000000*0.05;
+         double Salariofinal;
+         
+         //Proceso
+         Salariofinal= SaludMagica+ProteccionDePension;
+        
+         //Salida
+          System.out.println(" Tu saldo final es " + Salariofinal + " monedas de oro ¡Preparate para tu proxima aventura! ");
+         
+         
+         
+        
     }
     
 }
